53bd451d7cc2d339b20d573f7c80d35cbf83d520
Adam Smith <hagansmith@gmail.com>
2018-02-10 11:58:45 -0600

    inital setup and first passing test

---
+++ RomanNumeralConverter.App/NumberConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralConverter.App
+{
+    public class NumberConverter
+    {
+        public string Convert (int number)
+        {
+            string result = "";
+            var character = "I";
+
+            if (number == 1)
+            {
+              return result = character; 
+            }
+
+            return result;
+        }
+    }
+}
---
+++ RomanNumeralConverter.Test/RomanNumeralTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using RomanNumeralConverter.App;
+
+namespace RomanNumeralConverter.Test
+{
+    [TestFixture]
+    public class RomanNumeralTests
+    {
+        [Test]
+        public void Entering_the_number_1_results_in_roman_numeral_I()
+        {
+            // Arrange
+            var number = 1;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("I", result);
+        }
+    }
+}
