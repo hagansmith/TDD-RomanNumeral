f01575e20ae3dfa7959342561a4eb90eb0afc0f2
Adam Smith <hagansmith@gmail.com>
2018-02-11 15:03:16 -0600

    all tests pass

--- RomanNumeralConverter.App/NumberConverter.cs
+++ RomanNumeralConverter.App/NumberConverter.cs
@@ -41,64 +41,7 @@ namespace RomanNumeralConverter.App
                     number -= set.Key;
                 }
             }
-            //while (remainder > 0)
-            //{
-                
-                //if (romans.ContainsKey(remainder))
-                //{
-                //    if (result == "")
-                //    {
-                //        result = romans[remainder] + result;
-                //    }
-                //    else
-                //    {
-                //        result = result.Insert(result.Length - 1, romans[remainder]);
-                //    }
-                    
-                //    break;
-                //}
-                //else if (romans.ContainsKey(remainder - 1))
-                //{
-                //    result = result + romans[remainder - 1];
-                //    remainder = remainder - (remainder - 1);
-                //    result = result + romans[remainder];
-                //    remainder -= remainder; 
-                //    continue;
-                //}
-                //else if (!romans.ContainsKey(number))
-                //{
-                //    if (remainder < 5 )
-                //    {
-                //        result += new String('I' , remainder);
-                //        break;
-                //    }
-                //    else if (remainder < 10)
-                //    {
-                //        int index = romans.Keys.ToList().BinarySearch(remainder);
-                //        string value = romans.ElementAt(+~index).Value;
-                //        int key = romans.ElementAt(+~index).Key;
-                //        remainder = key - remainder;
-                //        result += value;
-                //    }
-                //    else if (remainder > 39)
-                //    {
-                //        int index = romans.Keys.ToList().BinarySearch(remainder);
-                //        string value = romans.ElementAt(+~index).Value;
-                //        int key = romans.ElementAt(+~index).Key;
-                //        remainder = key - remainder;
-                //        result += value;
-                //    }
-                //    else
-                //    {
-                //        int index = romans.Keys.ToList().BinarySearch(remainder);
-                //        string value = romans.ElementAt(~index -1).Value;
-                //        int key = romans.ElementAt(~index - 1).Key;
-                //        remainder = remainder - key;
-                //        result += value;
-                //    }
 
-                //}
-            //}
             return result;
         }
     }
--- RomanNumeralConverter.Test/RomanNumeralTests.cs
+++ RomanNumeralConverter.Test/RomanNumeralTests.cs
@@ -100,5 +100,185 @@ namespace RomanNumeralConverter.Test
             // Assert
             Assert.AreEqual("XLV", result);
         }
+
+        [Test]
+        public void Entering_the_number_68_results_in_roman_numeral_LXVIII()
+        {
+            // Arrange
+            var number = 68;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("LXVIII", result);
+        }
+
+        [Test]
+        public void Entering_the_number_83_results_in_roman_numeral_LXXXIII()
+        {
+            // Arrange
+            var number = 83;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("LXXXIII", result);
+        }
+
+        [Test]
+        public void Entering_the_number_97_results_in_roman_numeral_XCVII()
+        {
+            // Arrange
+            var number = 97;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("XCVII", result);
+        }
+
+        [Test]
+        public void Entering_the_number_99_results_in_roman_numeral_XCIX()
+        {
+            // Arrange
+            var number = 99;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("XCIX", result);
+        }
+
+        [Test]
+        public void Entering_the_number_500_results_in_roman_numeral_D()
+        {
+            // Arrange
+            var number = 500;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("D", result);
+        }
+
+        [Test]
+        public void Entering_the_number_501_results_in_roman_numeral_DI()
+        {
+            // Arrange
+            var number = 501;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("DI", result);
+        }
+
+        [Test]
+        public void Entering_the_number_649_results_in_roman_numeral_DCXLIX()
+        {
+            // Arrange
+            var number = 649;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("DCXLIX", result);
+        }
+
+        [Test]
+        public void Entering_the_number_798_results_in_roman_numeral_DCCXCVIII()
+        {
+            // Arrange
+            var number = 798;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("DCCXCVIII", result);
+        }
+
+        [Test]
+        public void Entering_the_number_891_results_in_roman_numeral_DCCCXCI()
+        {
+            // Arrange
+            var number = 891;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("DCCCXCI", result);
+        }
+
+        [Test]
+        public void Entering_the_number_1000_results_in_roman_numeral_M()
+        {
+            // Arrange
+            var number = 1000;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("M", result);
+        }
+
+        [Test]
+        public void Entering_the_number_1004_results_in_roman_numeral_MIV()
+        {
+            // Arrange
+            var number = 1004;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("MIV", result);
+        }
+
+        [Test]
+        public void Entering_the_number_1006_results_in_roman_numeral_MVI()
+        {
+            // Arrange
+            var number = 1006;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("MVI", result);
+        }
+
+        [Test]
+        public void Entering_the_number_1023_results_in_roman_numeral_MXXIII()
+        {
+            // Arrange
+            var number = 1023;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("MXXIII", result);
+        }
+
+        [Test]
+        public void Entering_the_number_2014_results_in_roman_numeral_MMXIV()
+        {
+            // Arrange
+            var number = 2014;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("MMXIV", result);
+        }
+
+        [Test]
+        public void Entering_the_number_3999_results_in_roman_numeral_MMMCMXCIX()
+        {
+            // Arrange
+            var number = 3999;
+            var numberConverter = new NumberConverter();
+            // Act
+            var result = numberConverter.Convert(number);
+            // Assert
+            Assert.AreEqual("MMMCMXCIX", result);
+        }
     }
 }
